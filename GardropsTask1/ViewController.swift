1d83e328d9a9e66f3123bbce87de6b5e27fee8f5
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-08-08 23:37:37 +0300

    Initial Commit

---
+++ GardropsTask1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GardropsTask1
+//
+//  Created by İbrahim Bayram on 8.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
